f510302cfc35aef116ed0d874b96c3ed758aae74
uriburg <uriburg@gmail.com>
2025-05-21 00:20:47 +0300

    Improve training
    Fix blocking transfer to GPU

--- tools/train.py
+++ tools/train.py
@@ -6,7 +6,8 @@ from torch.utils.data import DataLoader, random_split
 from loss.yolov1_loss import YOLOV1Loss
 from tqdm import tqdm
 
-
+device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
+print(f"Training on device {device}.")
 
 def load_config(args):
     # Read the config file #
@@ -20,32 +21,26 @@ def load_config(args):
 
 
 def collate_function(data):
-    return list(zip(*data))
+    batch = list(zip(*data))
+    yolo_targets = torch.cat([target['yolo_targets'].unsqueeze(0).float().to(device=device, non_blocking=True) for target in batch[1]], dim=0)
+    images = torch.cat([im.unsqueeze(0).float().to(device=device, non_blocking=True) for im in batch[0]], dim=0)
+    return yolo_targets, images
 
-
-def validation_epoch(validation_loader, device, use_sigmoid, model, loss_fn):
+def validation_epoch(validation_loader, use_sigmoid, model, loss_fn):
     running_vloss = 0.0
     with torch.no_grad():
-        for idx, (ims, targets, _) in enumerate(tqdm(validation_loader, leave=False)):  # tqdm(
-            yolo_targets = torch.cat([target['yolo_targets'].unsqueeze(0).float().to(device) for target in targets], dim=0)
-            im = torch.cat([im.unsqueeze(0).float().to(device) for im in ims], dim=0)
-            yolo_preds = model(im)
+        for idx, (yolo_targets, images) in enumerate(tqdm(validation_loader, leave=False)):  # tqdm(
+            yolo_preds = model(images)
             loss = loss_fn(yolo_preds, yolo_targets, use_sigmoid)
             running_vloss += loss.item()
 
     return running_vloss
 
 
-def train_epoch(train_loader, device, use_sigmoid, model, optimizer, loss_fn):
+def train_epoch(train_loader, use_sigmoid, model, optimizer, loss_fn):
     loss_train = 0.0
-    for idx, (ims, targets, _) in enumerate(tqdm(train_loader, leave=False)):  # tqdm(
-        # if not load_to_memory:
-        yolo_targets = torch.cat([target['yolo_targets'].unsqueeze(0).float().to(device) for target in targets], dim=0)
-        im = torch.cat([im.unsqueeze(0).float().to(device) for im in ims], dim=0)
-        # else:
-        #    yolo_targets = targets
-        #    im = ims
-        yolo_preds = model(im)
+    for idx, (yolo_targets, images) in enumerate(tqdm(train_loader, leave=False)):  # tqdm(
+        yolo_preds = model(images)
         loss = loss_fn(yolo_preds, yolo_targets, use_sigmoid)
         optimizer.zero_grad()
         loss.backward()
@@ -54,7 +49,7 @@ def train_epoch(train_loader, device, use_sigmoid, model, optimizer, loss_fn):
     return loss_train
 
 
-def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, dataset, save_path, use_sigmoid, device, load_to_memory,
+def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, dataset, save_path, use_sigmoid,
                   batch, num_workers):
     best_vloss = float('inf')
     train_size = int(0.90 * len(dataset))  # 90% for training
@@ -64,26 +59,20 @@ def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, dataset, save_
 
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    if load_to_memory:
-        train_loader = DataLoader(train_dataset, batch_size=batch, shuffle=True,
-                                   collate_fn=collate_function)
-        val_loader = DataLoader(val_dataset, batch_size=batch, shuffle=True,
-                                   collate_fn=collate_function)
-    else:
-        train_loader = DataLoader(train_dataset, batch_size=batch, shuffle=True,
+    train_loader = DataLoader(train_dataset, batch_size=batch, shuffle=True,
                                    collate_fn=collate_function,
                                    num_workers=num_workers, persistent_workers=True)
 
-        val_loader = DataLoader(val_dataset, batch_size=batch, shuffle=True,
+    val_loader = DataLoader(val_dataset, batch_size=batch, shuffle=True,
                                    collate_fn=collate_function,
                                    num_workers=num_workers, persistent_workers=True)
 
     for epoch in range(1, n_epochs + 1):
         model.train(True)
-        loss_train = train_epoch(train_loader, device, use_sigmoid, model, optimizer, loss_fn) / len(train_loader)
+        loss_train = train_epoch(train_loader, use_sigmoid, model, optimizer, loss_fn) / len(train_loader)
         scheduler.step()
         model.eval()
-        loss_validation = validation_epoch(val_loader, device, use_sigmoid, model, loss_fn) / len(val_loader)
+        loss_validation = validation_epoch(val_loader, use_sigmoid, model, loss_fn) / len(val_loader)
         if epoch % 3 == 0:
             print(f'{datetime.datetime.now().strftime("%H:%M:%S")} Epoch {epoch}, Training loss {loss_train:.3f}, Validation loss {loss_validation:.3f}')
         if loss_validation < best_vloss:
@@ -93,9 +82,6 @@ def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, dataset, save_
 
 
 def main(args):
-    #torch.multiprocessing.set_start_method('fork')
-    device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
-    print(f"Training on device {device}.")
     config = load_config(args)
     dataset_config = config['dataset_params']
     model_config = config['model_params']
@@ -128,11 +114,7 @@ def main(args):
 
     training_loop(n_epochs=train_config['num_epochs'], optimizer=optimizer, scheduler=scheduler, model=model,
                   loss_fn=criterion, dataset=voc, save_path=save_path, use_sigmoid=model_config['use_sigmoid'],
-                  device=device, load_to_memory=dataset_config['load_to_memory'], batch=train_config['batch_size'],
-                  num_workers=dataset_config['num_workers'])
-
-
-    pass
+                  batch=train_config['batch_size'], num_workers=dataset_config['num_workers'])
 
 
 if __name__ == '__main__':
